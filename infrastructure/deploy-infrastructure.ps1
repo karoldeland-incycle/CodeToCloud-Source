6b27b946fa3ceaf847933d5bc0754acd75944cfe
Karol Deland <99189951+karoldeland-incycle@users.noreply.github.com>
2022-02-08 08:57:11 -0500

    Update deploy-infrastructure.ps1

--- infrastructure/deploy-infrastructure.ps1
+++ infrastructure/deploy-infrastructure.ps1
@@ -1,14 +1,14 @@
 param
 (
-    [string] $studentprefix = "tst"
+    [string] $studentprefix = "ab6a6778fc"
 )
 
 $resourcegroupName = "fabmedical-rg-" + $studentprefix
 $cosmosDBName = "fabmedical-cdb-" + $studentprefix
 $webappName = "fabmedical-web-" + $studentprefix
 $planName = "fabmedical-plan-" + $studentprefix
-$location1 = "westeurope"
-$location2 = "northeurope"
+$location1 = "canadacentral"
+$location2 = "eastus2"
 $appInsights = "fabmedicalai-" + $studentsuffix
 
 #First create a group
